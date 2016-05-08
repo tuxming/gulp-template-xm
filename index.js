1607d41adac6060d7e57133bf240aba24cdacb52
tuxming <tuxming@gmail.com>
2016-05-08 17:39:52 +0800

    remove tpl comment

--- index.js
+++ index.js
@@ -28,96 +28,6 @@ module.exports = function(options){
 }
 
 function processHtmlForString(content, options){
-	/*
-	<!-- tpl {"parent": "index.tmp.html", "css": "/styles/user.css, /style/default.css", 
-		"title": "this is user profile page", "js": "/script/config.js, /scripts/user.js", 
-		//other variable
-	} -->
-	
-	variable: #{title}
-	
-	*/
-	
-	/*
-	index.tmp.html
-	
-	<!doctype html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>#{title}</title>
-		
-		<link rel="stylesheet" href="../styles/main.css">
-		<link rel="stylesheet" href="../styles/style.css">
-		
-		#{css}
-		
-	</head>
-	<body>
-		<!--build {"type":"tpl", "ref":"template/header.html"}-->
-		<!--endbuild-->
-		
-		#{content}
-		
-		<!--build {"type":"tpl", "ref":"template/footer.html"}-->
-		<!--endbuild-->
-		
-		<!-- build {"type":"bower" }-->
-		<!-- endbuild -->
-		
-	</body>
-	</html>
-	
-	user.html
-	<!-- tpl {"parent": "index.tmp.html", "css": "/styles/user.css, /style/default.css", 
-		"title": "this is user profile page", "js": "/script/config.js, /scripts/user.js", 
-		//other variable
-	} -->
-	<!--begin user page -->
-	<div class="container">
-		<h1>my profile</h1>
-		<span>github.com/tuxming</span>
-		.....
-	</div><!--end user page-->
-	
-	result user.html
-	<!doctype html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>this is user profile page</title>
-		
-		<link rel="stylesheet" href="../styles/main.css">
-		<link rel="stylesheet" href="../styles/style.css">
-		<link rel="stylesheet" href="/styles/user.css">
-		<link rel="stylesheet" href="/styles/default.css">
-		
-		
-	</head>
-	<body>
-		<!--build {"type":"tpl", "ref":"template/header.html"}-->
-		<!--endbuild-->
-		
-		<!--begin user page -->
-		<div class="container">
-			<h1>my profile</h1>
-			<span>github.com/tuxming</span>
-			.....
-		</div><!--end user page-->
-		
-		<!--build {"type":"tpl", "ref":"template/footer.html"}-->
-		<!--endbuild-->
-		
-		<!-- build {"type":"bower" }-->
-		<!-- endbuild -->
-		
-		<script type="text/javascript" src="/scripts/config.js"></script>
-		<script type="text/javascript" src="/scripts/user.js"></script>
-		
-	</body>
-	</html>
-	
-	*/
 	
 	/*
 	<!-- tpl {"parent": "index.tmp.html", "css": "/styles/user.css, /style/default.css", 
@@ -132,7 +42,10 @@ function processHtmlForString(content, options){
 	
 	var matchElems = content.match(tplReg);
 	if(matchElems && matchElems.length>0){
+		
 		var tpl = matchElems[0];
+		content = content.replace(tpl, "");
+		
 		var jsonStr = tpl.match(jsonReg);
 		
 		if(jsonStr && jsonStr.length>0){
